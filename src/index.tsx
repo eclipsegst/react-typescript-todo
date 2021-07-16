98e60794be2a439bb693ad43c44df9084c702418
Zhaolong Zhong <zhlgzh@gmail.com>
2021-07-16 00:05:19 -0700

    Added styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -2,29 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from "react-dom"
 import { Router, RouteComponentProps, Link } from "@reach/router"
-
 import TodoListScreen from "./TodoList";
 import TodoDetail from "./TodoDetail";
 import Home from "./Welcome";
 import Login from "./Login";
+import {Container} from "./styles";
+import GlobalStyle from "./styles/global"
+import './index.css'
 
 const App = () => (
-  <div>
-    <nav>
-      {/* Reference: https://reach.tech/router/example/basic */}
-      <Link to="/">Home</Link> {" | "}
-      <Link to="/login">Login</Link> {" | "}
-      <Link to="/todolist">Todo List</Link>
-      {/* <Link to="/tododetail/1">Todo Detail</Link> */}
-    </nav>
-    <Router>
-      <Home path="/" />
-      {/* Reference: https://reach.tech/router/typescript */}
-      <TodoListScreen path="/todolist" /> 
-      <TodoDetail path="/tododetail/:id" /> 
-      <Login path="/login" /> 
-    </Router>
-  </div>
+  <>
+  <GlobalStyle />
+  {/* <Container> */}
+    <div>
+      <nav>
+        {/* Reference: https://reach.tech/router/example/basic */}
+        <Link to="/">Home</Link> {" | "}
+        <Link to="/login">Login</Link> {" | "}
+        <Link to="/todolist">Todo List</Link>
+        {/* <Link to="/tododetail/1">Todo Detail</Link> */}
+      </nav>
+      <Router>
+        <Home path="/" />
+        {/* Reference: https://reach.tech/router/typescript */}
+        <TodoListScreen path="/todolist" /> 
+        <TodoDetail path="/tododetail/:id" /> 
+        <Login path="/login" /> 
+      </Router>
+    </div>
+  {/* </Container> */}
+  </>
 );
 
 ReactDOM.render(
