cd9300795d33172e6dfa5bbc9bfb0233d80871dc
Zhaolong Zhong <zhlgzh@gmail.com>
2021-07-17 19:30:13 -0700

    Updated login page

--- src/Login/index.tsx
+++ src/Login/index.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { FunctionComponent } from "react";
 import { RouteComponentProps } from "@reach/router";
+import { Form } from "./style";
 
 const Login: FunctionComponent<RouteComponentProps> = () => {
   return(
-    <form>
-      <label>
-        <p>Username</p>
-        <input type="text" />
-      </label>
-      <label>
-        <p>Password</p>
-        <input type="password" />
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+    <Form>
+      <form>
+        <label>
+          <p>Username</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </Form> 
   )
 };
 
